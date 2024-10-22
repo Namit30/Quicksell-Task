b63f0124f18ba4d6f12efc4547f2cf883ea00824
Namit Mehrotra <namit752@gmail.com>
2024-10-23 01:50:25 +0530

    initial commit

---
+++ src/assets/images.js
@@ -0,0 +1,36 @@
+import fade_circle from './new/To_do.svg';
+import plus from './new/add.svg';
+import dashed_circle from './new/Backlog.svg';
+import cancel from './new/Cancelled.svg';
+import display_filter from './new/Display.svg';
+import done from './new/Done.svg';
+import three_dot from './new/dotmenu.svg';
+import down from './new/down.svg';
+import grey_exclamation from './new/greyexclamation.svg';
+import high from './new/Imghighpriority.svg';
+import in_progress from './new/inprogress.svg';
+import low from './new/lowpriority.svg';
+import medium from './new/mediumpriority.svg';
+import no_priority from './new/no_priority.svg';
+import void_circle from './new/To_do.svg';
+import urgent from './new/yellowexclamation.svg';
+
+
+export {
+    cancel,
+    dashed_circle,
+    done,
+    down,
+    fade_circle,
+    grey_exclamation,
+    high,
+    in_progress,
+    low,
+    medium,
+    urgent,
+    plus,
+    no_priority,
+    three_dot,
+    display_filter,
+    void_circle
+}
